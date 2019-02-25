b1611eff8349e9c1a92561ba319b7e63211ebcbc
njeriigitau <mitchelgitau@gmail.com>
2019-02-25 12:37:03 +0300

    modify scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,13 @@
-var answers=[]
-var quizs=[1,2,3,4,5]
-var scores=[]
-var average=0
-var outputs
+$(document).ready(function(){
+  $("input[type='button']").click(function(event){
+    event.preventDefault();
+    var question1 = parseInt($("input:radio[name = 1]:checked").val());
+    var question2 = parseInt($("input:radio[name = 2]:checked").val());
+    var question3 = parseInt($("input:radio[name = 3]:checked").val());
+    var question4 = parseInt($("input:radio[name = 4]:checked").val());
+    var question5 = parseInt($("input:radio[name = 5]:checked").val());
+
+    var total =1 + 2 + 3 + 4 + 5;
+    $("form").hide();
+    $("button").hide();
+    $("#result").text("Your score is" + result +"out of 30");
